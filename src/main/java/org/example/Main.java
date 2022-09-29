f1e394a373e052d979f6ba152b0c31ca1bc695b7
Osama Atef <55250306+osamaeraqe@users.noreply.github.com>
2022-09-29 15:45:49 +0200

    Second Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,28 +1,34 @@
 package org.example;
 
+import org.example.Rover.Coordinate;
 import org.example.Rover.MarsRover;
 import org.example.execution.BaseExecution;
 import org.example.execution.executionFactory;
+import org.example.obsticle.Obsticle;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int n,m, q ;
-        String x = "";
-        String s = "";
+        int x,y ;
+        String direction = "";
+        String CommandString = "";
+        List < Obsticle>obsticleList = new ArrayList<>();
         Scanner scan = new Scanner(System.in);
-        n = scan.nextInt();
-        m = scan.nextInt();
-        x = scan.next();
-        s = scan.next();
-
+        x = scan.nextInt();
+        y = scan.nextInt();
+        direction = scan.next();
+        CommandString = scan.next();
+        obsticleList.add(new Obsticle(2,3, "NORTH"));
+        obsticleList.add(new Obsticle(7,5, "EAST"));
 
         BaseExecution execution = executionFactory.getExecInstance();
 
-        //MarsRover rover =  execution.solve(n,m,x,s);
+        MarsRover rover =  execution.solve(x ,y , direction , CommandString , obsticleList);
 
-     //   System.out.println(rover.getCoordinate().getX() + " " + rover.getCoordinate().getY() + " " + rover.getDirection().name());
+        System.out.println(rover.getCoordinate().getX() + " " + rover.getCoordinate().getY() + " " + rover.getDirection().name());
 
     }
 }
